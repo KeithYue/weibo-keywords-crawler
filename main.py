ae0037efc0d314922bacca6f61ac629db6f134cd
KeithYue <buaawangyue@gmail.com>
2014-07-18 15:52:57 +0800

    execution point main

---
+++ main.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+import random
+import logging
+from weibo_crawler import WeiboCrawler
+from multiprocessing.dummy import Pool as ThreadPool
+
+def load_keywords(path='./keywords.txt'):
+    keywords = []
+    with open(path, 'r') as f:
+        for line in f:
+            keywords.append(line.strip())
+
+    return keywords
+
+def load_users(path='./passwd.txt'):
+    '''
+    load the validate users
+    '''
+    users = []
+    with open(path, 'r') as f:
+        for line in f:
+            users.append(tuple(line.strip().split(None)))
+    print(users)
+    return users
+
+def crawl(keywords):
+    try:
+        wc = WeiboCrawler(keywords, *random.choice(g_users))
+        wc.crawl(50, comments = False)
+    finally:
+        wc.save()
+    return True
+
+def main():
+    logging.basicConfig(filename='./weibo_crawler.log', filemode='w', level=logging.INFO)
+    logging.info('Craler started..')
+    pool = ThreadPool(len(g_keywords))
+    results = pool.map(crawl, g_keywords)
+    pool.close()
+    pool.join()
+    logging.info('Crawler finished.')
+
+g_users = load_users()
+g_keywords = load_keywords()
+
+
+if __name__ == '__main__':
+    main()
